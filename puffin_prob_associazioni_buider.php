b3e51d71617329a31a49c27a020ab3ba229db3c1
Mastro <7125332+Mas7ro@users.noreply.github.com>
2024-10-14 18:17:55 +0200

    Create puffin_prob_associazioni_buider.php

---
+++ puffin_prob_associazioni_buider.php
@@ -0,0 +1,117 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+// Variabili per la connessione al database (da configurare)
+$host = "Database_Host";
+$username = "username";
+$password = "password"; // Sostituisci con la tua password
+$db_name = "database";
+
+// Creazione della connessione al database
+try {
+    $conn = new mysqli($host, $username, $password, $db_name);
+    if ($conn->connect_error) {
+        die("Connessione fallita: " . $conn->connect_error);
+    } else {
+        //echo "Connessione al database riuscita!";
+    }
+} catch (Exception $e) {
+    echo "Errore durante la connessione: " . $e->getMessage();
+}
+
+// Carica il contenuto della pagina
+//$url = 'https://www.fantacalcio.it/voti-fantacalcio-serie-a';
+$url = 'https://www.fantacalcio.it/probabili-formazioni-serie-a';
+$html = file_get_contents($url);
+
+// Crea un nuovo documento DOM
+$doc = new DOMDocument();
+libxml_use_internal_errors(true); // Supprime gli avvisi
+$doc->loadHTML($html);
+
+// Seleziona tutti gli elementi con la classe specificata
+$xpath = new DOMXPath($doc);
+$elements = $xpath->query('//a[@class="player-name player-link"]');
+
+// Inizializza un array per i risultati
+$giocatori = [];
+
+// Itera sugli elementi trovati
+foreach ($elements as $element) {
+    $href = $element->getAttribute('href');
+    // Estrai gli ultimi 3 elementi del link (puoi personalizzare questa parte)
+    $parti = explode('/', $href);
+    $squadra = $parti[count($parti) - 3];
+    $nomeCompleto = $parti[count($parti) - 2];
+    //echo($nomeCompleto."<br>");
+    // Rimuovi i trattini e le lettere successive SOLO se seguono una parola di più di 3 caratteri
+    $nomeParti = explode('-', $nomeCompleto);
+    $nome = '';
+    foreach ($nomeParti as $parte) {
+        if (strlen($parte) > 3 || $nome === '') {
+            $nome .= $parte . '-';
+        }
+    }
+    // Sostituisci TUTTI i trattini nel nome finale con spazi
+    $nome = str_replace('-', ' ', $nome);
+
+    $codice = $parti[count($parti) - 1];
+
+    // Aggiungi i dati all'array dei giocatori
+    $giocatori[] = [
+        'squadra' => $squadra,
+        'nome' => $nome,
+        'codice' => $codice
+    ];
+    //var_dump($giocatori);
+}
+// Eseguiamo la query per ottenere i dati dal database
+$sql = "SELECT Giocatore, Squadra, Cod FROM `fantamaniaci_revo`.`revo_stats`;";
+$result = $conn->query($sql);
+
+// Creazione di un array associativo per memorizzare i giocatori del database
+$giocatoriDB = [];
+while ($row = $result->fetch_assoc()) {
+    $giocatoriDB[$row['Giocatore']] = $row;
+}
+
+// Confronto tra i giocatori dell'array e quelli del database (con ricerca parziale)
+//echo "<table>";
+//echo "<tr><th>Codice</th><th>Nome Array</th><th>Codice DB</th><th>Nome DB</th><th>Squadra</th></tr>";
+$conta=0;
+foreach ($giocatori as $cod => $giocatore) {
+    
+    $nomeCompleto = trim($giocatore['nome']); // Rimuovi spazi iniziali e finali
+    $trovato = false;
+
+    // Iniziamo dalla stringa completa e accorciamo progressivamente
+    for ($i = strlen($nomeCompleto); $i > 0; $i--) {
+        $nomeParziale = substr($nomeCompleto, 0, $i);
+        $searchPattern = "{$nomeParziale}";
+
+        foreach ($giocatoriDB as $nomeDB => $datiGiocatore) {
+
+            if (stripos(trim($nomeDB), $searchPattern) !== false) { // Rimuovi spazi anche nel nome del DB
+                
+                echo ("\$arrAssociazioni[{$conta}]=array({$giocatore['codice']},\"".ucfirst(trim($giocatore['nome']))."\",{$datiGiocatore['Cod']},\"{$datiGiocatore['Giocatore']}\",\"{$datiGiocatore['Squadra']}\"");
+                echo");<br>";
+                $conta ++;
+                $trovato = true;
+                break 2; // Esci da entrambi i cicli se troviamo una corrispondenza
+            }
+        }
+
+        if ($trovato) {
+            break; // Esci dal ciclo esterno se abbiamo trovato una corrispondenza
+        }
+    }
+
+    if (!$trovato) {
+        echo "Giocatore {$giocatore['nome']} non trovato nel database (motivo: {$nomeCompleto})<br>";
+    }
+}
+//echo "</table>";
+
+// Chiusura della connessione al database
+$conn->close();
